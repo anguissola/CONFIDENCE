01a9c946472068e4abfab6bba222e9578e7ed17a
ANDY MILLS <SM21BM@FSU.EDU>
2023-10-25 11:07:24 -0400

    getting somewhere --  tutorial w/ placeholder art

--- README.md
+++ README.md
@@ -1,13 +1,6 @@
 # p5play template
 
-This is a template repo that includes the following libraries:
 
-* [p5 & p5.sound](https://p5js.org/download/)
-* [p5play & planck](https://github.com/quinton-ashley/p5play)
-
-See the links for license information.
-
-`sketch.js` has a very simple game to demonstrate a few features of the p5play library. (Since it's hosted via GitHub Pages, you can [play it here](https://fsudigitalmedia.github.io/p5play_template/)). Wipe it out and make your own game! (After you record your high score, of course.)
 
 ## Notes
 
--- sketch.js
+++ sketch.js
@@ -1,64 +1,36 @@
-// a simple game where you click to drop a ball and 
-// try to get as many balls as possible on the spinning shape
-// move the mouse up and down to change the rotation speed of the shape
-// use the right and left keys to make the shape larger or smaller
+let backgroundImg;
+let suit1Img, suit2Img;
+let suit1, suit2;
 
-let spinningShape;
-let highScore = 0;
+function preload() {
+    backgroundImg = loadImage("backgroundImg.png");
+    suit1Img = loadImage("suitImg.png");
+    suit2Img = loadImage("suit2Img.png");
+}
 
 function setup() {
-    let canvas = new Canvas("fullscreen");
+    new Canvas(1436,727);
 
-    world.gravity.y = 10;
-
-    // this sprite can be created on a single line, but it's easier to read this way:
-	spinningShape = new Sprite();
-	spinningShape.width = canvas.width/5;
-	spinningShape.height = spinningShape.width;
-    spinningShape.collider = "kinematic";
-
-    textFont("Courier", 24);
+    suit1 = new Sprite();
+    suit1.img = suit1Img;
+    suit1.position = createVector(750,450);
+    suit1.drag = 10;
 }
 
 function draw() {
-
-    // try the game without this line :)
     clear();
+    image(backgroundImg,0,0)
 
-    // the map function translates a value from one range to another
-    // https://p5js.org/reference/#/p5/map
-    spinningShape.rotationSpeed = map(mouse.y, 0, canvas.height, -10, 10);
-
-    // create a ball when the mouse is clicked
-    // https://p5play.org/learn/input_devices.html
-    if (mouse.presses()) {
-        let ball = new Sprite(mouse.x, -20, 20);
-        // make the ball resist rolling when it touches the spinning shape
-        // https://p5play.org/learn/sprite.html?page=9
-        ball.rotationDrag = 10;
+    //handle dragging    
+    if (suit1.mouse.dragging()) {
+        suit1.moveTowards(
+            mouseX + suit1.mouse.x,
+            mouseY +suit1.mouse.y,
+            1)
+        console.log("dragging suit1")
     }
 
-    // make the spinning shape larger or smaller using the keyboard
-    // note the difference between presses (above) and pressing (here)
-    if (kb.pressing('right')) {
-        spinningShape.width += 10;
-    } else if (kb.pressing('left')) {
-        spinningShape.width -= 10;
-    }
+    //snap into position
 
-    // loop through the allSprites array and see how many are above the center of the screen
-    let currentScore = 0;
-    for (let sprite of allSprites) {
-        if (sprite.y < canvas.height/2) {
-            currentScore++;
-            if (currentScore > highScore) {
-                highScore = currentScore;
-            }
-        }
-    }
-
-    // display the score (minus 1 so it doesn't count the spinning shape)
-    text("BALLS: " + (currentScore-1), 40, 60);
-    text("HIGH:  " + (highScore-1), 40, 86);
-
+    // if (dist(suit1.x,suit.y))
 }
