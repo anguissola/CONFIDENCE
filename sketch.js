e6b757c3cd5b84086c5ac4229373bb3004ed65dd
ANDY MILLS <SM21BM@FSU.EDU>
2023-11-13 11:06:58 -0500

    disaster with some working elements

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 let backgroundImages = [];
 let whichBackgroundImage = 0;
 let whiteShirtImg, suit2Img;
-let whiteShirt, suit2;
+let whiteShirt, whitePants;
+let redJacket; 
+let brownPants, sweater, blackPants, blackShoes, shortBoots, tallBoots;
+let textBox;
 let frankCenter;
 let mySound;
 
@@ -10,10 +13,17 @@ function preload() {
     backgroundImages[0] = loadImage("backgroundflat.png");
     backgroundImages[1] = loadImage("clouds.jpg");
     myGuy = loadImage("base.png");
+    textBox = loadImage ("text box.png");
     whiteShirtImg = loadImage("whiteShirt.png");
-    suit2Img = loadImage("suit2Img.png");
-    soundFormats("flac", "mp3", "ogg", "wav");
-    mySound=loadSound("rise.wav");
+    whitePantsImg = loadImage("whitePants.png");
+    redJacketImg = loadImage("redJacket.png");
+    // brownPantsImg = loadImage("brownPants.png");
+    // sweaterImg = loadImage("sweater.png");
+    // blackPantsImg = loadImage("blackPants.png");
+    // blackShoesImg = loadImage("blackShoes.png");
+    // shortBootsImg = loadImage ("shortBoots.png");
+    // tallBootsImg = loadImage ("tallBoots.png");
+    // suit2Img = loadImage("suit2Img.png");
 }
 
 function setup() {
@@ -24,12 +34,38 @@ function setup() {
     whiteShirt.img = whiteShirtImg;
     whiteShirt.position = createVector(750,420);
     whiteShirt.drag = 10;
+
+    whitePants = new Sprite();
+    whitePants.img = whitePantsImg;
+    whitePants.position = createVector(750,620);
+    whitePants.drag = 10;
+  
+
+    // whiteShirt.layer = 1;
+    // whitePants.layer = 2;
+    // redJacket.layer = 3;
+    whiteShirt.overlaps(whitePants);
+
+
+    redJacket = new Sprite();
+    redJacket.img = redJacketImg;
+    redJacket.position = createVector(850,420);
+
+    redJacket.overlaps(whiteShirt);
+    redJacket.overlaps(whitePants);
+
+    whiteShirt.rotationSpeed=0;
+    whitePants.rotationSpeed=0;
+    redJacket.rotationSpeed = 0;
+
+
 }
 
 function draw() {
     clear();
     image(backgroundImages[whichBackgroundImage], 0, 0);
     image(myGuy, 410, 70);
+    image(textBox, 30,55);
     
     //handle dragging    
     if (whiteShirt.mouse.dragging()) {
@@ -38,6 +74,22 @@ function draw() {
             mouseY +whiteShirt.mouse.y,
             1)
         console.log("dragging whiteShirt")
+    }
+
+    if (whitePants.mouse.dragging()) {
+        whitePants.moveTowards(
+            mouseX + whitePants.mouse.x,
+            mouseY +whitePants.mouse.y,
+            1)
+        console.log("dragging whitePants")
+    }
+
+    if (redJacket.mouse.dragging()) {
+        redJacket.moveTowards(
+            mouseX + redJacket.mouse.x,
+            mouseY +redJacket.mouse.y,
+            1)
+        console.log("dragging redJacket")
     }
 
     //snap into position + trigger message
